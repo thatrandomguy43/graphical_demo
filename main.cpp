686c6f9ee941567f85d51134fcd8d877c3aa396b
thatrandomguy43 <126349923+thatrandomguy43@users.noreply.github.com>
2023-12-06 11:11:48 +0100

    removed more unwanted files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,41 @@
 #include "imgui.h"
+#include "backends/imgui_impl_opengl3.h"
+#include "backends/imgui_impl_sdl2.h"
+#include "SDL_opengl.h"
+#include "SDL.h"
 
+#include <iostream>
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+    int SDLstatus = SDL_Init(SDL_INIT_EVERYTHING);
+    if (SDLstatus != 0) 
+    {
+        print(cout, "damn bruh sdl couldn't start, this the error: {}", SDL_GetError());
+        return 1;
+    }
+
+    //i have no idea what this code does, well i guess i kinda know
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Window* window = SDL_CreateWindow("hopefully you can read this", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+    SDL_GLContext opengl_context = SDL_GL_CreateContext(window);
+    SDL_GL_MakeCurrent(window, opengl_context);
+    SDL_GL_SetSwapInterval(1); // Enable vsync
+
+    ImGui::CreateContext();
+    ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL2_InitForOpenGL(window, opengl_context);
+    ImGui_ImplOpenGL3_Init();
+
+
+    ImGui::DestroyContext(ImGui::GetCurrentContext());
+    SDL_Quit();
+
     return 0;
 }
